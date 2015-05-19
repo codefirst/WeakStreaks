9504043bb6025a4de195639206808947e29ce119
mzp <mzpppp@gmail.com>
2015-05-20 07:19:43 +0900

    Initial Commit

---
+++ WeakStreaks/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeakStreaks
+//
+//  Created by mzp on 5/20/15.
+//  Copyright (c) 2015 mzp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
