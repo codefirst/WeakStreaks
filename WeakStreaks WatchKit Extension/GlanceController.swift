15421ff9c5c478d8fbaefa58b86174985398d5b8
banjun <banjun@gmail.com>
2015-05-31 17:45:53 +0900

    make background of straeks labels red on ending streaks

--- WeakStreaks WatchKit Extension/GlanceController.swift
+++ WeakStreaks WatchKit Extension/GlanceController.swift
@@ -9,7 +9,12 @@
 import WatchKit
 import Foundation
 
+
+private let endingStreaksColor = UIColor(hue: 0, saturation: 0.8, brightness: 0.8, alpha: 1.0)
+
+
 class GlanceController: WKInterfaceController {
+    @IBOutlet weak var streaksGroup: WKInterfaceGroup!
     @IBOutlet weak var currentStreaks: WKInterfaceLabel!
     @IBOutlet weak var streaksUnitLabel: WKInterfaceLabel!
     @IBOutlet weak var graph: WKInterfaceImage!
@@ -40,6 +45,9 @@ class GlanceController: WKInterfaceController {
             self.streaksUnitLabel.setText(weekStreaks == 1 ? "week" : "weeks")
             let image = ContributionsCalendar(data: data).draw(self.graphSize)
             self.graph.setImage(image)
+            
+            let onEndingStreaks = (weekStreaks == 0) // FIXME: weakStreaks > 0 かつ 今週コミットしないと途切れる，という状態があるはず
+            self.streaksGroup.setBackgroundColor(onEndingStreaks ? endingStreaksColor : UIColor.clearColor())
         }
     }
 
